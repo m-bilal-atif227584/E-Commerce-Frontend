9768941c6c5c4e93cba564e5bb418bd62fad104a
m bilal atif227584 <m.bilal227584@gmail.com>
2025-11-05 13:20:54 +0500

    update

--- src/config/api.js
+++ src/config/api.js
@@ -5,31 +5,9 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 export const api = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  // headers: {
+  //   'Content-Type': 'application/json',
+  // },
 });
-
-// Request interceptor
-// api.interceptors.request.use(
-//   (config) => {
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-// Response interceptor
-// api.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       // Redirect to login on unauthorized
-//       window.location.href = '/login';
-//     }
-//     return Promise.reject(error);
-//   }
-// );
 
 export default api;
